215c1c2662184c56c393654e109f9a231b393d1a
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-09-13 19:18:32 -0700

    ch4 examples completed

---
+++ ch4/issues/main.go
@@ -0,0 +1,23 @@
+// see page 112
+
+// Issues prints a table of Github issues matching the search terms.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopl.io/ch4/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("#%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
---
+++ ch4/issuesreport/main.go
@@ -0,0 +1,91 @@
+// See page 113.
+
+// Issuesreport prints a report of issues matching the search terms.
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"gopl.io/ch4/github"
+)
+
+//!+template
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+//!-template
+
+//!+daysAgo
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+//!-daysAgo
+
+//!+exec
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(templ))
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//!-exec
+
+func noMust() {
+	//!+parse
+	report, err := template.New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//!-parse
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+//!+output
+$ go build gopl.io/ch4/issuesreport
+$ ./issuesreport repo:golang/go is:open json decoder
+67 issues:
+----------------------------------------
+Number: 48298
+User:   dsnet
+Title:  encoding/json: add Decoder.DisallowDuplicateFields
+Age:    369 days
+----------------------------------------
+Number: 36225
+User:   dsnet
+Title:  encoding/json: the Decoder.Decode API lends itself to misuse
+Age:    999 days
+----------------------------------------
+Number: 42571
+User:   dsnet
+Title:  encoding/json: clarify Decoder.InputOffset semantics
+Age:    670 days
+----------------------------------------
+...
+//!-output
+*/
