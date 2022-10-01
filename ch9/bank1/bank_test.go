49219740c067836fc212f44cda00f993fa29628c
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-10-01 06:42:51 -0700

    Started ch9

---
+++ ch9/bank1/bank_test.go
@@ -0,0 +1,35 @@
+// see page 262
+
+package bank_test
+
+import (
+	"fmt"
+	"testing"
+
+	bank "gopl.io/ch9/bank1"
+)
+
+func TestBank(t *testing.T) {
+	done := make(chan struct{})
+
+	// Alice
+	go func() {
+		bank.Deposit(200)
+		fmt.Println("=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	// Bob
+	go func ()  {
+		bank.Deposit(100)
+		done <- struct{}{}
+	}()
+
+	// Wait for both transactions.
+	<-done
+	<-done
+
+	if got, want := bank.Balance(), 300; got != want {
+		t.Errorf("Balance = %d, Want %d", got, want)
+	}
+}
