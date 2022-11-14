bad8d7a4b88ee74a4451e388c320de3975fe67e0
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-13 21:51:35 -0700

    4.1 started

---
+++ exercises/ch4/4.1/popcount/popcount.go
@@ -0,0 +1,32 @@
+// Exercise 4.1
+// Page 84
+//
+// Prompt:
+// Write a function that counts the number of bits
+// that are different in two SHA256 hashes.
+// (See PopCount from Section 2.6.2.)
+
+// popcount
+package popcount
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] | byte(i&1)
+	}
+}
+
+// Popcount returns the population count 
+// (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
