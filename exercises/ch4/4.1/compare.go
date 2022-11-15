7ee7b2c71430fe470cdd65fc49134e9d7e90ce13
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-14 22:36:12 -0700

    4.1 done

--- exercises/ch4/4.1/compare.go
+++ exercises/ch4/4.1/compare.go
@@ -6,37 +6,45 @@
 // that are different in two SHA256 hashes.
 // (See PopCount from Section 2.6.2.)
 
-/* 
-// popcount
-package popcount
+// Development notes:
+// Solution and additional resources from
+// https://stackoverflow.com/questions/34966658/compare-bits-in-two-arrays
 
-// pc[i] is the population count of i.
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] | byte(i&1)
-	}
-}
-
-// Popcount returns the population count 
-// (number of set bits) of x.
-func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
-}
- */
-
-// Compare takes two SHA256 hashes and 
+// Compare takes two strings, hashes them using SHA256, and
 // counts the number of bits that are different.
 package main
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func CountBitDifference(h1, h2 *[sha256.Size]byte) int {
+	count := 0
+	// Loops through h1's index.
+	for i := range h1 {
+		// Var b = the 
+		for b := h1[i] ^ h2[i]; b != 0; b &= b - 1 {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
-	
+	// Inputs
+	s1 := "1"
+	s2 := "2"
+
+	// Compute hashes
+	h1 := sha256.Sum256([]byte(s1))
+	h2 := sha256.Sum256([]byte(s2))
+
+	// Calculate Difference
+	difference := CountBitDifference(&h1, &h2)
+
+	// Output
+	fmt.Printf("Input1: %s | Hash1: %X | Type: %T\n", s1, h1, h1)
+	fmt.Printf("Input2: %s | Hash2: %X | Type: %T\n", s2, h2, h2)
+	fmt.Printf("Number of different bits: %d\n", difference)
 }
