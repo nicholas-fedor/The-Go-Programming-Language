5c33a43ec1426d39007ea67257551393188d43da
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-15 14:17:02 -0700

    Generic working

--- exercises/ch4/4.4/rotate.go
+++ exercises/ch4/4.4/rotate.go
@@ -5,8 +5,32 @@
 // Write a version of rotate that operates in a single pass.
 
 // Rotate rotates a slice left by n elements.
+// Example:
+// input  = [0, 1, 2, 3, 4, 5]
+// output = [2, 3, 4, 5, 0, 1]
 package main
 
-func main() {
+import "fmt"
 
+func Rotate[T comparable](s[] T) {
+	first := 0
+	last := len(s) - 1
+
+	for first < last {
+		s[first], s[last] = s[last], s[first]
+		first++
+		last--
+	}
+
+	fmt.Println(s)
+}
+
+func main() {
+	input := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println(input)
+
+	// TODO:
+	// Still need to figure out algorithm that 
+	// conforms to the exercise requirements.
+	Rotate(input[:])
 }
