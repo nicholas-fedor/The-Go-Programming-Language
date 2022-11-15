e94adfe770609cfd7c9519a0d2fec0b85ee70474
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-15 01:35:47 -0700

    started 4.4

---
+++ exercises/ch4/4.4/rotate.go
@@ -0,0 +1,12 @@
+// Exercise 4.4
+// Page 93
+//
+// Prompt:
+// Write a version of rotate that operates in a single pass.
+
+// Rotate rotates a slice left by n elements.
+package main
+
+func main() {
+
+}
