7c3afb2b779c6a2f898d23a8751aefcaf59f3683
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-16 21:43:58 -0700

    4.6 started

---
+++ exercises/ch4/4.6/main.go
@@ -0,0 +1,14 @@
+// Exercise 4.6
+// Page 93
+//
+// Prompt:
+// Write an in-place function that squashes
+// each run of adjacent Unicode spaces
+// (see unicode.IsSpace) in a UTF-8 encoded
+// []byte slice into a single ASCII space.
+
+package main
+
+func main() {
+	
+}
