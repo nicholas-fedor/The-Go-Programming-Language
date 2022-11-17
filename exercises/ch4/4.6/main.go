0fa52943f26c53c997625714fde734c83921a1b9
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-16 22:31:57 -0700

    4.6 done

--- exercises/ch4/4.6/main.go
+++ exercises/ch4/4.6/main.go
@@ -9,6 +9,24 @@
 
 package main
 
+import (
+	"fmt"
+	"unicode"
+)
+
 func main() {
-	
+	// Input
+	s := []byte("a\tb\tc\td\te")
+	fmt.Printf("%s\n", s) // "a       b       c       d       e"
+	// Loop through each element of the slice.
+	for i := 0; i < len(s); i++ {
+		// Converts each element into a rune for unicode.IsSpace
+		v := rune(s[i])
+		// If the rune is a unicode space character, i.e. "/t"
+		if unicode.IsSpace(v) {
+			// Replaces the element with a single space.
+			s[i] = byte(' ')
+		}
+	}
+	fmt.Printf("%s\n", s) // "a b c d e"
 }
