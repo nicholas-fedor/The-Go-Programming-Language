291bc22001d241d041327535a9856758f7bbaaad
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-16 22:37:57 -0700

    4.7 started

---
+++ exercises/ch4/4.7/main.go
@@ -0,0 +1,14 @@
+// Exercise 4.7
+// Page 93
+// 
+// Prompt:
+// Modify reverse to reverse the characters 
+// of a []byte slice that represents a 
+// UTF-8 encoded string, in place. 
+// Can you do it without allocating new memory?
+
+package main
+
+func main() {
+	
+}
