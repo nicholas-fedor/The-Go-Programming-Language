11cdd94e168756d3846af686d7ea02857efa832b
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-11-30 15:26:55 -0700

    Exercise 4.7 done

--- exercises/ch4/4.7/main.go
+++ exercises/ch4/4.7/main.go
@@ -1,29 +1,55 @@
 // Exercise 4.7
 // Page 93
-// 
+//
 // Prompt:
-// Modify reverse to reverse the characters 
-// of a []byte slice that represents a 
-// UTF-8 encoded string, in place. 
+// Modify reverse to reverse the characters
+// of a []byte slice that represents a
+// UTF-8 encoded string, in place.
 // Can you do it without allocating new memory?
 
 // Development notes:
-// Minimization of memory allocation.
-// * minimize the amount of variable allocations.
+// Modifying the array, in place, means that the
+// underlying input array is modified during the call,
+// as opposed to moving the data into a new output array.
+//
+// This exercise builds upon example ch4/rev in that the
+// datatype is a UFT-8 encoded string, instead of integers.
+//
+// Used solution from https://github.com/kdama/gopl/blob/master/ch04/ex07/main.go
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	a := [...]int{0,1,2,3,4,5}
-	reverse(a[:])
-	fmt.Println(a) // "[5 4 3 2 1 0]"
+	// Input string
+	s := "Lorem ipsum"
+
+	// Remember to re-encode using string().
+	fmt.Println(string(reverseUTF8([]byte(s))))
+	// Output:
+	// "muspi meroL"
 }
 
-// reverse reverses a slice of ints in a place.
-func reverse(s []int)  {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+// reverseUTF8 iterates through a UTF-8 encoded string
+// and decodes each rune before reversing the string.
+func reverseUTF8(b []byte) []byte {
+	for i := 0; i < len(b); {
+		_, size := utf8.DecodeRune(b[i:])
+		reverse(b[i : i+size])
+		i += size
 	}
+	reverse(b)
+	return b
+}
+
+// reverse reverses a slice of bytes in place.
+func reverse(b []byte) []byte {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return b
 }
