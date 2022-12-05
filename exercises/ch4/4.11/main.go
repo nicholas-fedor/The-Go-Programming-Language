e7651fe348db17fdc6555c8fae3eab6a0a0c6791
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-12-05 16:55:35 -0700

    started 4.11

---
+++ exercises/ch4/4.11/main.go
@@ -0,0 +1,17 @@
+// Exercise 4.11
+// Page 112
+//
+// Prompt:
+// Build a tool that lets users create, read, update, and close GitHub issues from the command line,
+// invoking their preferred text editor when substantial text input is required.
+//
+// Development Notes:
+//
+
+// Program lets users work with GitHub issues from the command line.
+// Invokes the user's preferred text editor, if needed.
+package main
+
+func main() {
+	
+}
