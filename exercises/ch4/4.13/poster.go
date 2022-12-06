eda24341505369cd9371a60d4aef1150461a53d7
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-12-05 17:08:08 -0700

    started 4.12 and 4.13

---
+++ exercises/ch4/4.13/poster.go
@@ -0,0 +1,22 @@
+// Exercise 4.13
+// Page 113
+//
+// Prompt:
+// The JSON-based web service of the Open Movie Database lets you search https://omdbapi.com/ for a movie by name and download its poster image.
+// Write a tool poster that downloads the poster image for the movie named on the command line.
+//
+// Development Notes:
+//
+
+// Program poster downloads the poster image for a movie provided via the command line.
+package main
+
+func main() {
+	// Handle command-line input.
+	// Parse for movie name.
+	// Print back movie name to validate input.
+	// Once validated, pass to query.
+	// Generate API query.
+	// Handle response.
+	// Save image to program directory.
+}
