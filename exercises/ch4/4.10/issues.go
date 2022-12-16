6c9dd616102bb6ce170c65b060463c4b556d0f72
nicholas-fedor <71477161+nicholas-fedor@users.noreply.github.com>
2022-12-15 17:24:51 -0700

    4.10 done

--- exercises/ch4/4.10/issues.go
+++ exercises/ch4/4.10/issues.go
@@ -5,6 +5,13 @@
 // Modify issues to report the results in age categories,
 // say less than a month old, less than a year old, and more than a year old.
 
+// Development notes:
+// Used solution from here: 
+// https://github.com/linehk/gopl/blob/main/ch4/exercise4.10/main.go
+// 
+// I got stuck, prior to reviewing the above solution, with filtering and 
+// storing the data for later output handling.
+
 // Issues prints a table of GitHub issues matching the search terms.
 package main
 
@@ -17,21 +24,54 @@ import (
 	"gopl.io/example-problems/ch4/github"
 )
 
+type class string
+
+const (
+	LTOM class = "less than one month"
+	MTOM class = "more than one month"
+	LTOY class = "less than one year"
+	MTOY class = "more than one year"
+)
+
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
-	fmt.Println("Current Date/Time:", time.Now())
+
+	issueClass := make(map[class][]github.Issue)
+	cy, cm, _ := time.Now().Date()
 
 	for _, item := range result.Items {
-		// Age in days
-		age := time.Since(item.CreatedAt).Hours()/24
-		fmt.Printf("#%-5d %9.9s %s %.2f %.55s \n",
-			item.Number, item.User.Login, item.CreatedAt.Format("2006-01-02 15:05"), age, item.Title)
+		item := *item
+		y, m, _ := item.CreatedAt.Date()
 
+		switch {
+		// issue is <= 1 month.
+		case cm-m <= time.Month(1):
+			issueClass[LTOM] = append(issueClass[LTOM], item)
+			
+			// issue is > 1 month.
+		case cm-m > time.Month(1):
+			issueClass[MTOM] = append(issueClass[MTOM], item)
+			
+			// issue is <= 1 year.
+		case cy-y <= 1:
+			issueClass[LTOY] = append(issueClass[LTOY], item)
+			
+			// issue is > 1 year.
+		case cy-y > 1:
+			issueClass[MTOY] = append(issueClass[MTOY], item)
+		}
 	}
 
+	for class, issues := range issueClass {
+		fmt.Printf("Age Class: %s\n", class)
+		fmt.Printf("Item #\tUser:\tItem Summary:\n")
+		for _, item := range issues {
+			fmt.Printf("#%-5d\t%9.9s\t%.55s\n", item.Number, item.User.Login, item.Title)
+		}
+		fmt.Println()
+	}
 }
- 
