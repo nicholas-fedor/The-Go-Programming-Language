896f8451bd1f2815eae626327e2f6e348b36f6a7
Nick Fedor <71477164+nicholas-fedor@users.noreply.github.com>
2023-03-27 01:07:12 +0000

    Ex 5.1 created

---
+++ exercises/ch5/5.1/findlinks.go
@@ -0,0 +1,15 @@
+// Exercise 5.1
+// Page 124
+//
+// Prompt:
+// Change the findlinks program to traverse the n.FirstChild linked list using recursive calls to visit instead of a loop.
+
+// Development notes:
+// 
+
+// Program findlinks
+package main
+
+func main() {
+	
+}
