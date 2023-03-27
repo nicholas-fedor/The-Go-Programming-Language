e14d8460db6b90a88af8abc46dacbc9189223c86
Nick Fedor <71477164+nicholas-fedor@users.noreply.github.com>
2023-03-27 00:59:11 +0000

    Ex 4.14 created

---
+++ exercises/ch4/4.14/github.go
@@ -0,0 +1,15 @@
+// Exercise 4.14
+// Page 117
+//
+// Prompt:
+// Create a web server that queries GithHub once and then allows navigation of the list of bug reports, milestones, and users.
+//
+// Development Notes:
+// This is effectively a full-stack web app.
+
+// Program runs a web server that allows users to view GitHub data.
+package main
+
+func main() {
+	// 
+}
